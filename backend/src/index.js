8b2a2f0bd3ec35a8a2c868624dc0fb8a81705b5f
Striver-3110 <21csjay067@ldce.ac.in>
2025-01-17 16:15:42 +0530

    response issue solved!

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,12 +3,15 @@ import dotenv from "dotenv";
 import authRoutes from "./routes/auth.route.js";
 import { connectDB } from "./lib/db.js";
 
+import cors from "cors";
+
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT; 
 
 app.use(express.json());
+app.use(cors())
 
 app.use("/api/auth", authRoutes);
 
